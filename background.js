8259cce1f52f213273fa87d2414b320b8a1891d6
Blake Winton <bwinton@latte.ca>
2015-08-04 09:30:14 -0600

    Make eslint happy.

--- background.js
+++ background.js
@@ -1,11 +1,17 @@
+/* eslint-env browser */
+/* eslint no-console:0 */
+/* global chrome */
+
+'use strict';
+
 chrome.runtime.onConnect.addListener(function(port) {
-  console.log("onConnect", JSON.stringify(port.sender));
+  console.log('onConnect', JSON.stringify(port.sender));
 
   port.onMessage.addListener(function(msg) {
-    console.log("parent got msg", msg);
+    console.log('parent got msg', msg);
   });
 
   port.onDisconnect.addListener(function() {
-    console.log("port disconnected in parent");
+    console.log('port disconnected in parent');
   });
 });
--- content.js
+++ content.js
@@ -1,14 +1,20 @@
-document.body.style.backgroundColor = "red";
+/* eslint-env browser */
+/* eslint no-console:0 */
+/* global chrome */
 
-console.log("Content script");
+'use strict';
+
+document.body.style.backgroundColor = 'red';
+
+console.log('Content script');
 
 var port = chrome.runtime.connect();
-port.postMessage("first message");
+port.postMessage('first message');
 port.onMessage.addListener(function(msg) {
-  console.log("got port message", msg);
-  port.postMessage("another content msg");
+  console.log('got port message', msg);
+  port.postMessage('another content msg');
 });
 
 port.onDisconnect.addListener(function() {
-  console.log("port disconnected in child");
+  console.log('port disconnected in child');
 });
